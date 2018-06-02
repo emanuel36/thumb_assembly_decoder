44b94f58a2a37fd0405f9ca82397e9a141901b84
emanuel36 <emanuel_ec@alu.ufc.br>
2018-06-02 11:47:08 -0300

    add to repository

---
+++ src/read_file (Cópia em conflito de Robert Cabral 2016-11-26).c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "decoder.h"
+
+void read_file(){
+
+char *hexa;
+int num, instruction = 0, opcode_num, option, a = 0, contador = 0, i = 0;
+int valid = 0;
+
+hexa = (char *)calloc(10000000, sizeof(char));
+
+	FILE *arquivo;
+
+	arquivo = fopen("dados.txt","r");
+
+	if(arquivo == NULL){
+		printf("Arquivo não encontrado!\n");
+		exit(1);
+	}
+
+volta:
+	valid = 0, contador = 0, a = 0;
+	while(fgets(hexa, 10000000, arquivo) != NULL){
+    	/* writing content to stdout */
+    	//puts(hexa);
+
+		sscanf(hexa, "%x", &num);
+
+		
+
+	
+		for(i = 0; i < 10000000; i++){
+			if((hexa[i] == '0' || hexa[i] == '1' || hexa[i] == '2' || hexa[i] == '3' || hexa[i] == '4' || hexa[i] == '5' || 
+				hexa[i] == '6' || hexa[i] == '7' || hexa[i] == '8' || hexa[i] == '9' || hexa[i] == 'a' || hexa[i] == 'b' || 
+				hexa[i] == 'c' || hexa[i] == 'd' || hexa[i] == 'e' || hexa[i] == 'f' || hexa[i] == 'A' || hexa[i] == 'B' || 
+				hexa[i] == 'C' || hexa[i] == 'D' || hexa[i] == 'E' || hexa[i] == 'F' || hexa[i] == '\n')){
+
+				valid++;
+				if(valid == 5)
+					break;
+
+			}
+
+			else{
+				printf("Numero inválido.\n\n");				
+				goto volta;
+				break;
+			}
+		}
+
+		if(valid == 5){
+			printf("Numero valido\n");
+		}
+		valid = 0;
+
+	
+    		while(hexa[a] != '\n'){
+				contador++;
+				a++;
+			}
+
+			if(contador > 4 || contador < 4){
+				printf("Instrução inválida!\n");
+				printf("\n");	
+			}
+
+				else{ 
+
+   					printf("Value in hexa: %x\n", num);
+
+	   				opcode_num = opcode(instruction, num);
+	
+					printf("Decoded instruction:\n");
+					instruction_decoder(instruction, opcode_num, num);
+
+					printf("\n");
+
+				}
+
+			contador = 0;
+			a = 0;
+	}
+
+
+	free(hexa);
+
+	fclose(arquivo);
+}
